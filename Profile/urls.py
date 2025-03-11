ae5c3e59f3af2213faace077715189b1027051c8
xodyakov <tor4@slappytappy.bedlam>
2025-03-10 20:19:31 -0400

    frontend hardwork

--- Profile/urls.py
+++ Profile/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import *
 
@@ -7,5 +9,6 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('profile/', login_view, name='profile'),
     path('register/', RegisterUser.as_view(), name='register'),
-    path('', include('Main.urls')),
+    path('', include('Main.urls'))
+    ,
 ]
