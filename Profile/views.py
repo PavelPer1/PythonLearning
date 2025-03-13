d736d2c7a44ec08f6565990862c1d3615c5bc7d1
belyaev <lolhyh456@gmail.com>
2025-03-13 17:10:37 +0000

    fix teacher

--- Profile/views.py
+++ Profile/views.py
@@ -33,7 +33,7 @@ class RegisterUser(CreateView):
     def form_valid(self, form):
         user = form.save()
 
-        if self.request.POST.get('pointer') == 'Teacher':
+        if self.request.POST.get('pointer') == 'teacher':
             a = Teacher(name=user)
             a.save()
         else:
